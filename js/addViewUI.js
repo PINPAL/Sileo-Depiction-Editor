75400fd2e80334acc608ce82a3d859580d5cd9e5
PINPAL <josh.hayes614@gmail.com>
2019-07-28 18:36:51 +0100

    Add DepictionHeaderView UI

--- js/addViewUI.js
+++ js/addViewUI.js
@@ -1,15 +1,43 @@
 // Function called when user picks a view to add
 function addView(type) {
-    // Insert new view
-    config.tabs[currentViewingTab].views.splice(
-        newViewIndex, // Insert at index (variable is global from when user clicked "add new view" button)
-        0, // Delete 0 items
-        {"class":type} // Object to be inserted (type is passed when user chooses which view to add)
-    )
     switchAlertPage(
         "Edit View",
         "Customize View Content",
         "edit" + type,
-        ""
+        "saveNewView('" + type + "')"
     )
+}
+
+// Function called when user saves changes on addNewViewUI
+function saveNewView(type) {
+    let view = {"class":type}
+    let editUI = document.getElementById("edit" + type)
+    // Handle View
+    switch (type) {
+        case "DepictionHeaderView":
+            // Title
+            let title = "Heading Text"
+            if (editUI.getElementsByClassName("titleField")[0].value != "") {
+                title = editUI.getElementsByClassName("titleField")[0].value
+            }
+            view.title = title
+            // Set other properties
+            view.useMargins = editUI.getElementsByClassName("useMargins")[0].checked
+            view.useBoldText = editUI.getElementsByClassName("useBoldText")[0].checked
+            view.alignment = editUI.getElementsByClassName("alignment")[0].value
+            break;
+        default:
+            throw("View is not yet supported")
+    }
+
+    // Insert new view
+    config.tabs[currentViewingTab].views.splice(
+        newViewIndex, // Insert at index (variable is global from when user clicked "add new view" button)
+        0, // Delete 0 items
+        view // Object to be inserted
+    )
+    // Re-render Preview
+    renderSileoDepiction(config)
+    // Hide popup
+    hideAlert()
 }
